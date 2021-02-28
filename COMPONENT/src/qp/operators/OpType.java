a658237c3113c1cadcea269ff0f05c4e22d07e05
timothyleong97 <timothy.leong@u.nus.edu>
2021-02-28 09:24:22 +0800

    Implementing ExternalSort operator

--- COMPONENT/src/qp/operators/OpType.java
+++ COMPONENT/src/qp/operators/OpType.java
@@ -13,4 +13,6 @@ public class OpType {
     public static final int EXTERNAL_SORT = 4;
     public static final int DISTINCT = 5;
     public static final int ORDER_BY = 6;
+    public static final int AGGREGATE = 7;
+
 }
