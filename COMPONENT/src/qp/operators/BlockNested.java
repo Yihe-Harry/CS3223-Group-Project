e74748274e210047f1a0462736a64adb93a43035
DESKTOP-T6HFRHE\Yihe <wyharry628@gmail.com>
2021-02-28 17:20:43 +0800

    start implementing block nested join

---
+++ COMPONENT/src/qp/operators/BlockNested.java
@@ -0,0 +1,13 @@
+package qp.operators;
+
+import qp.utils.Attribute;
+import qp.utils.Batch;
+import qp.utils.Condition;
+import qp.utils.Tuple;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class BlockNested {
+}
