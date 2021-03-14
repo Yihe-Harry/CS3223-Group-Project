60ac308403e1c0748ccfb68a40cfb585964e3901
timothyleong97 <timothy.leong@u.nus.edu>
2021-03-14 16:52:55 +0800

    Remove typo in QueryMain

--- COMPONENT/src/QueryMain.java
+++ COMPONENT/src/QueryMain.java
@@ -177,7 +177,7 @@ public class QueryMain {
         while ((resultbatch = root.next()) != null) {
 //            System.out.println("Result batch: ");
 //            Debug.PPrint(resultbatch);
-            for (int i = 0; i 1< resultbatch.size(); ++i) {
+            for (int i = 0; i < resultbatch.size(); ++i) {
                 printTuple(resultbatch.get(i));
             }
         }
