ee9f737d4fdca0fd7fa5f77015bb120668b1ee3f
Andy Lam <andyylam@gmail.com>
2021-03-16 10:30:43 +0800

    Remove todo

--- COMPONENT/src/qp/operators/JoinType.java
+++ COMPONENT/src/qp/operators/JoinType.java
@@ -7,12 +7,9 @@
 package qp.operators;
 
 public class JoinType {
-
-    // TODO Fix back original values
-    public static final int NESTEDJOIN = 0; //0
+    public static final int NESTEDJOIN = 0;
     public static final int BLOCKNESTED = 1;
-    public static final int SORTMERGE = 2; //2
-    public static final int HASHJOIN = 3;
+    public static final int SORTMERGE = 2;
 
     public static int numJoinTypes() {
         return 1;
