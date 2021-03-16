2f0b47bbb4ad47d0e38041436e5080ca31f38199
Andy Lam <andyylam@gmail.com>
2021-03-16 10:30:24 +0800

    All join queries 3-7 working for SortMerge

--- COMPONENT/src/qp/operators/JoinType.java
+++ COMPONENT/src/qp/operators/JoinType.java
@@ -9,9 +9,9 @@ package qp.operators;
 public class JoinType {
 
     // TODO Fix back original values
-    public static final int NESTEDJOIN = 2; //0
+    public static final int NESTEDJOIN = 0; //0
     public static final int BLOCKNESTED = 1;
-    public static final int SORTMERGE = 0; //2
+    public static final int SORTMERGE = 2; //2
     public static final int HASHJOIN = 3;
 
     public static int numJoinTypes() {
